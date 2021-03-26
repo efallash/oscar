1e74f53bd259779d0481a49fa63f30fa52189b65
efallash <efallashdez@gmail.com>
2021-03-25 21:37:02 -0600

    Multiple changes:
    -Added packages:
    	-oscar_bringup: Launch file for the platform
    	-oscar_common: Scripts and general services
    	-oscar_msgs: Saving and compiling services and topic messages
    -Added sphere and box for testing different objects
    -Renamed poses in the moveit config
    -Developed change_hands_test.py as an example of a "change of hands" pick and place sequence

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ To do:
 - Programar servicios:
     - Para ir a posicion xy en la mesa a la altura de pregrasp (seleccionando el brazo)
     - Para bajar a la posicion de grasp, cerrar el gripper y subir a la altura del pregrasp
-    - Para mover los objetos del experimento en la mesa (servicio /gazebo/set_model_state
+    - Para mover los objetos del experimento en la mesa (servicio /gazebo/set_model_state)
---
+++ oscar_common/scripts/change_hands_test.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+
+# Python 2 compatibility imports
+from __future__ import absolute_import, division, print_function
+from future import standard_library
+
+import sys, rospy, tf, moveit_commander, random
+from geometry_msgs.msg import Pose, Point, Quaternion
+from math import pi
+
+#from thor_msgs.srv import GripperControl, GripperControlRequest, GripperControlResponse 
+
+#Script to pick a box, go to home position and place it in its original position
+
+
+#End effector orientations
+orient = Quaternion(*tf.transformations.quaternion_from_euler(0, pi/4, 0))
+orient1 = Quaternion(*tf.transformations.quaternion_from_euler(0, pi/4, pi/4))
+orient2 = Quaternion(*tf.transformations.quaternion_from_euler(0, pi/4, -pi/4))
+
+#Predefined poses for the end effector
+pose_pick = Pose(Point( 0.4, -0.35, 0.8), orient)
+pose_place = Pose(Point( 0.25, 0, 0.801), orient1)
+pose_pick_left = Pose(Point( 0.25, 0, 0.801), orient2)
+pose_place_left = Pose(Point( 0.4, 0.35, 0.8), orient)
+
+#Predefined poses for the end effector
+pose_pre_pick = Pose(Point( 0.4, -0.35, 0.9), orient)
+pose_pre_place = Pose(Point( 0.25, 0, 0.9), orient1)
+pose_pre_pick_left = Pose(Point( 0.25, 0, 0.9), orient2)
+pose_pre_place_left = Pose(Point( 0.4, 0.35, 0.9), orient)
+
+
+
+
+#Start Moveit
+moveit_commander.roscpp_initialize(sys.argv)
+#Start Node
+rospy.init_node('oscar_grab_test',anonymous=True)
+#Commander for the arm and for the robot
+right = moveit_commander.MoveGroupCommander("right_arm")
+left = moveit_commander.MoveGroupCommander("left_arm")
+right_gripper = moveit_commander.MoveGroupCommander("right_gripper")
+left_gripper = moveit_commander.MoveGroupCommander("left_gripper")
+robot = moveit_commander.RobotCommander()
+
+
+
+#Home Right
+rospy.loginfo("Going Upright")    
+right.set_named_target("upright")
+print(right.go(wait=True)) 
+
+#Home Left
+rospy.loginfo("Going Upright")    
+left.set_named_target("upright")
+print(left.go(wait=True)) 
+
+
+###RIGHT PICK AND PLACE
+
+
+#Gripper
+rospy.loginfo("Open Gripper")    
+right_gripper.set_named_target("open")
+print(right_gripper.go(wait=True)) 
+
+#Pre Grasp Position
+rospy.loginfo("Pre Pick")
+right.set_pose_target(pose_pre_pick)
+print(right.go(wait=True))
+
+
+#Pre Grasp Position
+rospy.loginfo("Pick")
+right.set_pose_target(pose_pick)
+print(right.go(wait=True))
+
+#Gripper
+rospy.loginfo("Open Gripper")    
+right_gripper.set_named_target("closed")
+print(right_gripper.go(wait=True)) 
+
+
+#Home
+rospy.loginfo("Home")    
+right.set_named_target("home")
+print(right.go(wait=True)) 
+
+
+#Pre Grasp Position
+rospy.loginfo("Pre Place")
+right.set_pose_target(pose_pre_place)
+print(right.go(wait=True))
+
+
+#Pre Grasp Position
+rospy.loginfo("Place")
+right.set_pose_target(pose_place)
+print(right.go(wait=True))
+
+
+#Gripper
+rospy.loginfo("Open Gripper")    
+right_gripper.set_named_target("open")
+print(right_gripper.go(wait=True)) 
+
+
+#Pre Grasp Position
+rospy.loginfo("Pre Place")
+right.set_pose_target(pose_pre_place)
+print(right.go(wait=True))
+
+
+#Home
+rospy.loginfo("Home")    
+right.set_named_target("home")
+print(right.go(wait=True)) 
+
+
+### LEFT PICK AND PLACE
+
+#Gripper
+rospy.loginfo("Open Gripper")    
+left_gripper.set_named_target("open")
+print(left_gripper.go(wait=True)) 
+
+#Pre Grasp Position
+rospy.loginfo("Pre Pick")
+left.set_pose_target(pose_pre_pick_left)
+print(left.go(wait=True))
+
+
+#Pre Grasp Position
+rospy.loginfo("Pick")
+left.set_pose_target(pose_pick_left)
+print(left.go(wait=True))
+
+#Gripper
+rospy.loginfo("Open Gripper")    
+left_gripper.set_named_target("closed")
+print(left_gripper.go(wait=True)) 
+
+
+#Home
+rospy.loginfo("Home")    
+left.set_named_target("home")
+print(left.go(wait=True)) 
+
+
+#Pre Grasp Position
+rospy.loginfo("Pre Place")
+left.set_pose_target(pose_pre_place_left)
+print(left.go(wait=True))
+
+
+#Pre Grasp Position
+rospy.loginfo("Place")
+left.set_pose_target(pose_place_left)
+print(left.go(wait=True))
+
+
+#Gripper
+rospy.loginfo("Open Gripper")    
+left_gripper.set_named_target("open")
+print(left_gripper.go(wait=True)) 
+
+
+#Pre Grasp Position
+rospy.loginfo("Pre Place")
+left.set_pose_target(pose_pre_place_left)
+print(left.go(wait=True))
+
+
+#Home
+rospy.loginfo("Home")    
+left.set_named_target("home")
+print(left.go(wait=True)) 
+
+
+
+
+
+moveit_commander.roscpp_shutdown()
